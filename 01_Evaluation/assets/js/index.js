e1da4aa59449d58036f89b156c11bd65234290ca
Ami1308 <amisha9565@gmail.com>
2022-09-26 11:28:29 +0530

    adding my first assessment

---
+++ 01_Evaluation/assets/js/index.js
@@ -0,0 +1,21 @@
+// const s=document.getElementById("toggle");
+// function menub(){
+//     if(s.style.display==="none"){
+//         s.style.display="block";
+//     }
+//     else{
+//         s.style.display="none";
+//     }
+
+// }
+
+function menu(){
+    let s = document.getElementById("navbar")
+    if(s.style.display != "none")
+    {  
+       s.style.display="none";
+    }
+    else{
+       s.style.display="block";
+    }
+}
